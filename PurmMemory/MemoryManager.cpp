4a583ccc1b153c26ffb1853c7645f121c3b3a930
Purm <purm-dev@secure-mail.biz>
2012-06-02 23:10:04 +0200

    cleaned up some code

--- PurmMemory/MemoryManager.cpp
+++ PurmMemory/MemoryManager.cpp
@@ -9,14 +9,15 @@ namespace PurmMemory {
 
 	MemoryManager::~MemoryManager(void)
 	{
-#if(_DEBUG)
-		::OutputDebugString(_T("MemoryManager::~MemoryManager(): Deleting Object Instance\r\n"));
-#endif
+		#if(_DEBUG)
+			::OutputDebugString(_T("MemoryManager::~MemoryManager(): Deleting Object Instance\r\n"));
+		#endif
+
 		this->CloseProcess();
 
-#if(_DEBUG)
-		::OutputDebugString(_T("MemoryManager::~MemoryManager(): Object Instance deleted\r\n"));
-#endif
+		#if(_DEBUG)
+			::OutputDebugString(_T("MemoryManager::~MemoryManager(): Object Instance deleted\r\n"));
+		#endif
 	}
 
 	//opens a process for reading/writing/other stuff
@@ -24,8 +25,17 @@ namespace PurmMemory {
 		this->_processHandle = ::OpenProcess(processRights, false, processId);
 		if(!this->_processHandle) {
 			::SetLastError(MemoryManager::_ERROR_OPENPROCESS_FAILED);
+			#if(_DEBUG)
+				::OutputDebugString(_T("MemoryManager::OpenProcess(...): OpenProcess returned false\r\n"));
+			#endif
 			return false;
 		}
+
+		#if(_DEBUG)
+			else {
+				::OutputDebugString(_T("MemoryManager::OpenProcess(...): OpenProcess returned true\r\n"));
+			}
+		#endif
 
 		return true;
 	}
@@ -33,13 +43,14 @@ namespace PurmMemory {
 	//closes the process handle
 	bool MemoryManager::CloseProcess() {
 		BOOL success = ::CloseHandle(this->_processHandle);
-#if(_DEBUG)
-		if(!success) {
-			::OutputDebugString(_T("MemoryManager::CloseProcess(): CloseProcess returned false\r\n"));
-		} else {
-			::OutputDebugString(_T("MemoryManager::CloseProcess(): CloseProcess returned true\r\n"));
-		}
-#endif
+		#if(_DEBUG)
+			if(!success) {
+				::OutputDebugString(_T("MemoryManager::CloseProcess(): CloseProcess returned false\r\n"));
+			} else {
+				::OutputDebugString(_T("MemoryManager::CloseProcess(): CloseProcess returned true\r\n"));
+			}
+		#endif
+
 		this->_processHandle = NULL;
 		return (success != 0);
 	}
