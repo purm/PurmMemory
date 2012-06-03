a8b7f474771c6fb2f92839deffccbb00d09d1970
Purm <purm-dev@secure-mail.biz>
2012-06-03 13:52:51 +0200

    added example Dll for testing InjectDLl

---
+++ PurmMemoryDemoInjectionDll/dllmain.cpp
@@ -0,0 +1,23 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+#include <tchar.h>
+#include <Windows.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBox(NULL, _T("hello world"), _T("from injected dll"), 0);
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
